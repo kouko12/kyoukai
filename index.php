88619c055593dee969e1215026e94d8f583c10f7
kouko12 <wilson.ho.12@cnu.edu>
2014-12-31 17:41:00 -0500

    Add index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+
+		<!-- jQuery CSS -->
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+
+		<!-- FontAwesome used for vector(scalable) icons -->
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery-1.10.2.min.js"</script>
+
+		<!-- jQuery UI -->
+		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"</script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	</head>
+	
+	<body>
+		<h1>
+			Hello World!
+		
+			<!-- fa, icon name, fa-size -->
+			<i class="fa fa-paper-plane fa-5x"></i>
+		</h1>
+		
+	</body>
+	
+</html>
